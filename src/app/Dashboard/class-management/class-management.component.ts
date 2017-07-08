fdfb23a87009c49be0d8a2431eaf28c67ea0fb8e
DaNRoxXx <danroxxx@gmail.com>
2017-07-08 21:47:00 +0500

    Section can now be assigned to classes

--- src/app/Dashboard/class-management/class-management.component.ts
+++ src/app/Dashboard/class-management/class-management.component.ts
@@ -12,16 +12,20 @@ import 'rxjs/add/operator/toPromise';
 export class ClassManagementComponent implements OnInit {
 
   response: String;
+  response_2: String;
   getCampuses: any[];
   getClasses: any[];
+  getSections: any[];
   editRow: any;
 
   constructor(private router: Router, private http: Http) { }
 
   addClass(clName: HTMLInputElement, clFee: HTMLInputElement, Campus: any): void {
     let self = this;
+    let DropdownList = (document.getElementById("campus")) as HTMLSelectElement;
+    let SelectedIndex = DropdownList.selectedIndex;
     //console.log(Campus.value);
-    this.http.post("http://localhost:3000/campuses/class", { name: clName.value, fee: clFee.value, campid: Campus.value },
+    this.http.post("http://localhost:3000/campuses/class", { name: clName.value, fee: clFee.value, campId: Campus.value },
       new Headers({ 'Content-type': 'application/json' })).toPromise().then(function (res) {
         if (res.status === 201) {
           document.getElementById('response').className = "alert alert-success";
@@ -29,12 +33,34 @@ export class ClassManagementComponent implements OnInit {
           setTimeout(function () {
             self.showClasses();
           }, 500);
-          //self.showClasses();
         }
       })
+    if (SelectedIndex != 0) {
+      clName.value = "";
+      clFee.value = "";
+      DropdownList.selectedIndex = 0;
+    }
+  }
 
-    clName.value = "";
-    clFee.value = "";
+  addSection(sName: HTMLInputElement, Class: any): void {
+    let self = this;
+    let DropdownList = (document.getElementById("class")) as HTMLSelectElement;
+    let SelectedIndex = DropdownList.selectedIndex;
+    //console.log(Campus.value);
+    this.http.post("http://localhost:3000/classes/section", { name: sName.value, classId: Class.value },
+      new Headers({ 'Content-type': 'application/json' })).toPromise().then(function (res) {
+        if (res.status === 201) {
+          document.getElementById('response_2').className = "alert alert-success";
+          self.response_2 = "Section successfully added.";
+          setTimeout(function () {
+            self.showSections();
+          }, 500);
+        }
+      })
+    if (SelectedIndex != 0) {
+      sName.value = "";
+      DropdownList.selectedIndex = 0;
+    }
   }
 
   showClasses() {
@@ -52,6 +78,14 @@ export class ClassManagementComponent implements OnInit {
       .toPromise().then(function (res) {
         self.getCampuses = res.json();
         //console.log(self.getCampuses);
+      })
+  }
+
+  showSections() {
+    let self = this;
+    this.http.get("http://localhost:3000/classes/sections/get", new Headers({ 'Content-type': 'application/json' }))
+      .toPromise().then(function (res) {
+        self.getSections = res.json();
       })
   }
 
@@ -119,6 +153,7 @@ export class ClassManagementComponent implements OnInit {
   ngOnInit() {
     this.showCampuses();
     this.showClasses();
+    this.showSections();
   }
 
 }
